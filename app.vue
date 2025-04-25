fdfe45a2bf6340e6533192f940222b6c084addc8
FextraBlitz <lolanamol@gmail.com>
2025-04-26 00:26:47 +0800

    initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import NavBar from './comoponents/NavBar.vue';
+
+</script>
+
+<template>
+  
+  <div class="bg-radial from-violet-600 to-indigo-600 h-[200lvh]">
+    <NavBar />
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="w-full text-center sticky">
+        <span class="text-green-400">Hello World!</span>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
